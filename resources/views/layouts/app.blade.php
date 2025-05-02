fc33f6f922b667effa61a450d8bbd157477d0873
Jaka-Github <h1101231033@student.untan.ac.id>
2025-05-03 00:21:27 +0700

    memperbaiki css agar bisa di extend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,9 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alatku</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('resources/app.css') }}">
     
     <!-- Link ke file CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])

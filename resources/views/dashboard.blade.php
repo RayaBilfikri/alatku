fbd92ef815b686cfdfd2f1572d22154a63aac805
Jaka-Github <h1101231033@student.untan.ac.id>
2025-05-04 18:48:26 +0700

    perbaikan css pada dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('resources/app.css') }}">
+    
 </head>
 <body class="bg-gray-100 font-sans">
 

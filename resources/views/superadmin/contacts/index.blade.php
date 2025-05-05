faedfb776aa9af4e60028b08490a38ee18846f80
abimainurriza <aabimainurriza@gmail.com>
2025-05-05 16:02:07 +0700

    CRUD kontak terbaru

--- resources/views/superadmin/contacts/index.blade.php
+++ resources/views/superadmin/contacts/index.blade.php
@@ -34,9 +34,9 @@
                 <a href="#" class="block py-2 px-3 rounded hover:bg-orange-100">Kelola Produk</a>
                 <a href="#" class="block py-2 px-3 rounded hover:bg-orange-100">Kelola Ulasan</a>
                 <a href="#" class="block py-2 px-3 rounded hover:bg-orange-100">Kelola Carousel</a>
-                <a href="#" class="block py-2 px-3 rounded bg-orange-100 font-semibold">Kelola Kontak</a>
+                <a href="/contacts" class="block py-2 px-3 rounded bg-orange-100 font-semibold">Kelola Kontak</a>
                 <a href="#" class="block py-2 px-3 rounded hover:bg-orange-100">Kelola Cara Membeli</a>
-                <a href="#" class="block py-2 px-3 rounded hover:bg-orange-100">Kelola Profile Website</a>
+                <a href="/websiteprofiles" class="block py-2 px-3 rounded hover:bg-orange-100">Kelola Profile Website</a>
             </nav>
         </div>
     </aside>

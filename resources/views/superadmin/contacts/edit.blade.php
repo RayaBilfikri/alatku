93e9177bf763521cd466298fbcb7fe7145aae681
abimainurriza <aabimainurriza@gmail.com>
2025-05-06 18:15:43 +0700

    Penyesuaian components untuk header

--- resources/views/superadmin/contacts/edit.blade.php
+++ resources/views/superadmin/contacts/edit.blade.php
@@ -16,16 +16,7 @@
     <!-- Main content -->
     <main class="flex-1 bg-gray-50 p-6">
         <!-- Header -->
-        <div class="flex justify-end items-center h-20 border-b mb-4">
-            <div class="flex items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                     viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M5.121 17.804A13.937 13.937 0 0112 15c2.485 0 4.797.654 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0z"/>
-                </svg>
-                <span>Nama Akun</span>
-            </div>
-        </div>
+        @include('partials.header')
 
         <!-- Form Edit Kontak -->
         <div class="bg-white p-6 rounded shadow-md w-full lg:max-w-7xl mx-auto">

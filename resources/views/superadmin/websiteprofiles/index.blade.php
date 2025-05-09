fd27225b5ad8df02cf983a4038c0e8e6f4522871
abimainurriza <aabimainurriza@gmail.com>
2025-05-09 17:16:52 +0700

    penyesuaian warna tombol cancel

--- resources/views/superadmin/websiteprofiles/index.blade.php
+++ resources/views/superadmin/websiteprofiles/index.blade.php
@@ -101,7 +101,7 @@
     <!-- Modal for viewing logo -->
     <div id="logoModal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 hidden">
         <div class="bg-white p-6 rounded">
-            <button onclick="closeModal()" class="text-gray-500">X</button>
+        <button onclick="closeModal()" class="text-gray-500 hover:text-red-500">X</button>
             <img id="modalLogo" src="" alt="Logo" class="max-w-xs mx-auto mt-4">
         </div>
     </div>

03a1f69493c11d03c2aec82601aa70cbbe66d916
Jaka Sampoerna <h1101231033@student.untan.ac.id>
2025-05-10 16:20:45 +0700

    change location to indonesia (general)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -274,7 +274,7 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                                             </svg>
-                                            Tangerang, Indonesia
+                                            Indonesia
                                         </div>
                                         <div class="flex justify-between mb-3">
                                             <div class="bg-[#525FE1] text-white text-xs font-medium px-7 py-1 rounded-full">

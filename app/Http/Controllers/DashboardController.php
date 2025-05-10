f5d1b36ef51f896b8df8d4aa0cabcd84342c1815
AbiyyuAzkar <h1101231050@student.untan.ac.id>
2025-05-10 15:39:17 +0700

    fix login

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     Public function index()
     {
-        return view('/dashboard');
+        $user = Auth::user();
+
+        if ($user->role === 'superadmin') {
+            return view('/dashboard');
+        }
+
+        return redirect('/');
     }
 }

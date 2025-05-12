bd63778807f89435f207414310a5d1dae59d52b3
abimainurriza <aabimainurriza@gmail.com>
2025-05-13 02:55:31 +0700

    pembaruan CRUD kontak

--- resources/views/superadmin/contacts/create.blade.php
+++ resources/views/superadmin/contacts/create.blade.php
@@ -38,7 +38,7 @@
                 <div class="flex justify-start space-x-4">
                     <button type="submit"
                             class="bg-blue-600 hover:bg-grey-500 text-white px-6 py-3 rounded-md">
-                        Tambahkan
+                        Tambah
                     </button>
                     <a href="{{ route('superadmin.contacts.index') }}"
                        class="bg-red-500 hover:bg-grey-500 text-white px-6 py-3 rounded-md">

396aafe9cc5986fc3171bd984bfd7b818aaf8d6c
Jaka Sampoerna <h1101231033@student.untan.ac.id>
2025-06-10 19:26:40 +0700

    yml fix 5

--- app/Console/Commands/FixExceptionViews.php
+++ app/Console/Commands/FixExceptionViews.php
@@ -11,20 +11,27 @@ class FixExceptionViews extends Command
      *
      * @var string
      */
-    protected $signature = 'app:fix-exception-views';
+    protected $signature = 'fix:exception-views';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Fix exception views compilation issues with Bootstrap';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $this->info('Fixing exception views...');
+        
+        // Add your fix logic here
+        // Example: Copy or modify exception blade files
+        
+        $this->info('Exception views fixed successfully!');
+        
+        return Command::SUCCESS;
     }
 }

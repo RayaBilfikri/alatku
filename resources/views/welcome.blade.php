c953ac355145166034da08028707b201ac5fd05a
Jaka Sampoerna <h1101231033@student.untan.ac.id>
2025-06-10 14:34:47 +0700

    loading eager

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -547,7 +547,7 @@
                     style="will-change: transform, opacity;"
                 >
                     <!-- Background -->
-                    <img :src="slide.gambar" alt="Alatku Banner" class="absolute inset-0 w-full h-full max-w-full max-h-full object-cover z-0 rounded-3xl">
+                    <img :src="slide.gambar" loading="eager" alt="Alatku Banner" class="absolute inset-0 w-full h-full max-w-full max-h-full object-cover z-0 rounded-3xl">
                     <div class="absolute inset-0 bg-[#FFA41B]/60 z-0 rounded-3xl"></div>
 
                     <!-- Content -->
